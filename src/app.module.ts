762459469b8db5849a773b0570300e9af4ddf343
EstebanRuizz <rubianruiz05@my.com>
2024-06-11 05:20:59 -0500

    deleted import at AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
-import { HttpControllerModule } from './presentation/http-controller.module';
-import { PersistenceModule } from './infrastructure/persistence/persistence.module';
-
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    PersistenceModule,
-    HttpControllerModule,
   ],
   providers: [],
 })
